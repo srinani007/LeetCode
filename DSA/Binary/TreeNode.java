20cf017a3f9b2df99cf605157af9506eeee5caf8
srinani007 <kvsprasanth007@gmail.com>
2024-10-06 21:31:29 -0400

    BinaryTree

---
+++ DSA/Binary/TreeNode.java
@@ -0,0 +1,30 @@
+package Binary;
+
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+    TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+        this.left = left;
+         this.right = right;
+     }
+}
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int d_l = maxDepth(root.left);
+        int d_R = maxDepth(root.right);
+
+        return Math.max(d_l,d_R)+1;
+
+        
+    }
+}

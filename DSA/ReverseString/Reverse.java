af066a94cf0f835ff89bf149acfac0125f89c9d9
srinani007 <kvsprasanth007@gmail.com>
2024-10-06 22:41:46 -0400

    ReverseString

---
+++ DSA/ReverseString/Reverse.java
@@ -0,0 +1,24 @@
+package ReverseString;
+
+public class Reverse {
+    public void reverseString(char[] s){
+        int i=0;
+        int j=s.length-1;
+        while(i<j){
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+    }
+
+    
+
+    public static void main(String[] args) {
+        Reverse r=new Reverse();
+        char[] s={'h','e','l','l','o'};
+        r.reverseString(s);
+        System.out.println(s);
+    }
+}

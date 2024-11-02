ac7de3ff83155967a754fdbf382c90bb05bc97e0
srinani007 <kvsprasanth007@gmail.com>
2024-11-02 09:19:54 -0400

    nov2nd919

--- DSA/Dictionary/dupi.java
+++ DSA/Dictionary/dupi.java
@@ -1,19 +1,30 @@
 package Dictionary;
 
-class dupi{
-    public boolean containsDuplicate(int[] nums){
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] == nums[j]){
-                    return true;
-                }
+import java.util.HashSet;
+
+class Duplicates {
+    public boolean containsDuplicate(int[] nums) {
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] == nums[j]) {
+        //             return true; // Found a duplicate
+        //         }
+        //     }
+        // }
+        // return false; // No duplicates found
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if (set.contains(num)) {
+                return true;
             }
-            return false;
+            set.add(num);
         }
+        return false;
     }
+
     public static void main(String[] args) {
-        dupi d = new dupi();
-        int[] nums = {1,2,3,1};
-        System.out.println(d.containsDuplicate(nums));
+        Duplicates d = new Duplicates();
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(d.containsDuplicate(nums)); // Output: true
     }
 }

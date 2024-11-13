efe84ca074f142a5b098d6cedb427ae2d04ee5c5
srinani007 <kvsprasanth007@gmail.com>
2024-11-13 03:14:34 -0500

    g

--- DSA/BinaryTree/PreINOrder.java
+++ DSA/BinaryTree/PreINOrder.java
@@ -14,18 +14,19 @@ class TreeNode {
 }
 
 public class PreINOrder {
-    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
         Map<Integer, Integer> inOrderIndexMap = new HashMap<>();
-        for (int i = 0; i < inorder.length; i++) {
+        for(int i = 0; i < inorder.length; i++){
             inOrderIndexMap.put(inorder[i], i);
         }
-        return splitTree(preorder, inOrderIndexMap, 0, 0, inorder.length - 1);
+        return splitTree(preorder, inOrderIndexMap, 0, 0, inorder.length - 1); 
     }
-
-    private static TreeNode splitTree(int[] preorder, Map<Integer, Integer> inOrderIndexMap, int rootIndex, int left, int right) {
-        if (left > right) {
-            return null; // Base case: no elements to construct the tree
-        }
+    private TreeNode splitTree(int[] preorder, Map<Integer, Integer> inOrderIndexMap, 
+                           int rootIndex, int left, int right) {
+            // Base case: if there are no elements to construct the tree
+            if (left > right) {
+                return null;
+            }
 
         TreeNode root = new TreeNode(preorder[rootIndex]);
         int mid = inOrderIndexMap.get(preorder[rootIndex]);
@@ -34,19 +35,16 @@ public class PreINOrder {
         root.left = splitTree(preorder, inOrderIndexMap, rootIndex + 1, left, mid - 1);
         
         // Recursively build the right subtree
-        root.right = splitTree(preorder, inOrderIndexMap, rootIndex + mid - left + 1, mid + 1, right);
+        root.right = splitTree(preorder, inOrderIndexMap, rootIndex + (mid - left) + 1, mid + 1, right);
         
         return root;
     }
-
     public static void main(String[] args) {
+        PreINOrder solution = new PreINOrder();
         int[] preorder = {3, 9, 20, 15, 7};
         int[] inorder = {9, 3, 15, 20, 7};
-        TreeNode root = buildTree(preorder, inorder);
-        System.out.println(root.val); // 3
-        System.out.println(root.left.val); // 9
-        System.out.println(root.right.val); // 20
-        System.out.println(root.right.left.val); // 15
-        System.out.println(root.right.right.val); // 7
+        TreeNode root = solution.buildTree(preorder, inorder);
+        System.out.println(root);
+        
     }
 }

a33d9d7b0c7c9aa146c8166f4fcd23dbe00fe245
srinani007 <kvsprasanth007@gmail.com>
2024-11-20 01:14:58 -0500

    Message1123

---
+++ DSA/BinaryTree/FlattenTree.java
@@ -0,0 +1,55 @@
+package BinaryTree;
+
+
+
+
+public class FlattenTree {
+
+    
+    public static void main(String[] args) {
+        // Contructing the binary Tree
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(6);
+        // Flatten the binary tree
+        FlattenTree solution = new FlattenTree();
+        solution.flatten(root);
+        // print the flatten tree
+        solution.printFlattenedTree(root);
+    }
+    public void flatten(TreeNode root){
+        if(root == null) return;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        // set the left child to null
+        root.left = null;
+
+        //Recursively flatten the left and right subtrees
+        flatten(left);
+        flatten(right);
+        // connect right subtree
+        root.right = left;
+
+        // find the end of new right subtree
+        TreeNode cur = root;
+        while(cur.right != null) cur = cur.right;
+
+        // connect the original right subtree
+        cur.right = right;
+    }
+
+    // function to print flattened tree
+    public void printFlattenedTree(TreeNode root){
+        while(root != null){
+            System.out.println(root.val + " -> ");
+            root = root.right;
+        }
+        System.out.println("null");
+    }
+    
+    
+}
---
+++ DSA/sortingSe/frequency.java
@@ -0,0 +1,54 @@
+package sortingSe;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+
+public class frequency {
+    @SuppressWarnings("unused")
+    public List<List<String>> groupAnagrams(String[] str){
+        /* Map<String, List<String>> map = new HashMap<>();
+        for (String s : str) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String stored = new String(charArray);
+
+            map.putIfAbsent(stored, new ArrayList<>());
+            map.get(stored).add(s);
+        }
+        return new ArrayList<>(map.values());*/
+
+        /*Map<String, List<String>> map = new HashMap<>();
+        for (String s : str) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String stored = new String(charArray);
+            if(!map.containsKey(stored)){
+                map.put(stored, new ArrayList<>());
+            }
+            map.get(stored).add(s);
+        }
+        return new ArrayList<>(map.values());*/
+
+       /*Map<String, List<String>> map = new HashMap<>();
+        for (String s : str) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String stored = new String(charArray);
+            map.computeIfAbsent(stored, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());*/
+        Map<String, List<String>> map = new HashMap<>();
+        
+        
+    }
+    public static void main(String[] args) {
+        frequency solution = new frequency();
+        String[] str = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = solution.groupAnagrams(str);
+        System.out.println(result);
+    }
+}

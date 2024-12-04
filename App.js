ae7acfede2b585db05730cbed4b2a94b8f08e397
srinani007 <kvsprasanth007@gmail.com>
2024-12-04 05:42:12 -0500

    new

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(){
+    return (
+        <div className="App">
+            <h1>JWT Authentication Demo</h1>
+            <login/>
+        </div>
+    );
+}
+export default App;
